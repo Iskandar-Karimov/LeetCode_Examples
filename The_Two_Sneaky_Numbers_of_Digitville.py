f607de627d5d4762c3c5dddbae41b926b6774020
Iskandar-Karimov <iskandarworkofficial@gmail.com>
2025-01-26 05:53:34 +0300

    Improved Sneaky Numbers of Digitville

--- The_Two_Sneaky_Numbers_of_Digitville.py
+++ The_Two_Sneaky_Numbers_of_Digitville.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        """
         seen = []
         dupes = []
         for i in range(len(nums)):
@@ -12,3 +13,13 @@ class Solution(object):
             else:
                 dupes.append(nums[i])
         return dupes
+        """
+
+        seen = {}
+        dupes = []
+        for i in range(len(nums)):
+            if nums[i] not in seen:
+                seen[nums[i]] = i
+            else:
+                dupes.append(nums[i])
+            return dupes
